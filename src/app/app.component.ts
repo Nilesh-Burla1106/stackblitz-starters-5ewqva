3eea1c1a767c411b43d4849d536e459f97df2504
Nilesh-Burla1106 <141109890+Nilesh-Burla1106@users.noreply.github.com>
2023-08-10 14:53:46 +0530

    4 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
+
 
 @Component({
   standalone:true,
   selector: 'app-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports:[HeroesComponent]
+  
 })
 export class AppComponent implements OnInit {
-   username:string="Nilesh Burla";
-  mobile:string='8308997054';
-  age:number=20;
+  /* username:string="Prathamesh";
+  mobile:string='7058013272';
+  age:number=19;
+  avatar:string='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80'*/
+  title = 'Tour of Heroes';
   
   constructor() { }
 

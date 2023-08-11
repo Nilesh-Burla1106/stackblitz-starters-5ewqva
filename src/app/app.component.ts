1d99420353fd8ec49f1991821459f4e0d5e1ac48
Nilesh-Burla1106 <141109890+Nilesh-Burla1106@users.noreply.github.com>
2023-08-11 15:00:57 +0530

    2 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HEROES } from './mock-heroes';
 
@@ -9,7 +10,7 @@ import { HEROES } from './mock-heroes';
   selector: 'app-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports:[HeroesComponent,CommonModule]
+  imports:[HeroesComponent,CommonModule,HeroDetailComponent]
   
 })
 export class AppComponent implements OnInit {
@@ -19,7 +20,7 @@ export class AppComponent implements OnInit {
   avatar:string='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80'*/
   title = 'Tour of Heroes';
   // heroes=HEROES;
-  
+
   
   constructor() { }
 

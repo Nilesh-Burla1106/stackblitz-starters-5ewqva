2632d7b73aefb709771deb547e096ae21a80e4d7
Nilesh-Burla1106 <141109890+Nilesh-Burla1106@users.noreply.github.com>
2023-08-11 14:40:24 +0530

    2 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,12 +1,12 @@
 import { Hero } from './hero';
 export const HEROES: Hero[] = [
-  { id: 1, name: 'Paras' },
-  { id: 2, name: 'Sourabh' },
-  { id: 3, name: 'Moksha' },
-  { id: 4, name: 'Jinesh' },
-  { id: 5, name: 'Nilesh' },
-  { id: 6, name: 'Pragati' },
-  { id: 7, name: 'Sakshi' },
-  { id: 8, name: 'Prathmesh' },
-  { id: 9, name: 'Amaan' }
+  { id: 1, name: 'Paras'},
+  { id: 2, name: 'Sourabh'},
+  { id: 3, name: 'Moksha'},
+  { id: 4, name: 'Jinesh'},
+  { id: 5, name: 'Nilesh'},
+  { id: 6, name: 'Pragati'},
+  { id: 7, name: 'Sakshi'},
+  { id: 8, name: 'Prathmesh'},
+  { id: 9, name: 'Amaan'}
 ]
